dce04675b667eb0b68f02e0c54d6cc0baadf6f4a
AlessandroMondin <alessandromondin00@gmail.com>
2024-03-27 11:29:32 +0100

    colored logging for cleared log messages

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import colorlog
 import logging
 import re
 import traceback
@@ -5,21 +6,37 @@ import traceback
 from functools import wraps
 
 
+def _get_console_handler(logger_level: int) -> colorlog.StreamHandler:
+    # Create a console handler using colorlog
+    console_handler = colorlog.StreamHandler()
+    formatter = colorlog.ColoredFormatter(
+        "%(fg_black)s%(bg_white)s%(name)s %(asctime)s%(reset)-4s"
+        " %(log_color)s[%(levelname)s]: %(log_color)s%(message)s",
+        datefmt="%H:%M:%S",
+        reset=True,
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red, bg_white",
+        },
+        style="%",
+    )
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logger_level)
+    return console_handler
+
+
 def get_logger(logger_name: str, logger_level: int = logging.DEBUG) -> logging.Logger:
     # Configure logger
-    logger = logging.getLogger(logger_name)  # Use the provided logger_name
-    logger.setLevel(logger_level)  # Use the provided logger_level
-    # Prevent the logger from propagating messages to ancestor loggers
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logger_level)
     logger.propagate = False
 
     # Check if logger already has handlers
     if not logger.handlers:
-        ch = logging.StreamHandler()
-        ch.setLevel(logger_level)
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-        ch.setFormatter(formatter)
+        ch = _get_console_handler(logger_level)
         logger.addHandler(ch)
 
     return logger
